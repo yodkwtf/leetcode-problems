8ea9291b79dce34a35872a5475d90364ef904af9
Durgesh <48durgesh.kumar@gmail.com>
2024-03-28 00:21:44 +0530

    Time: 134 ms (58.70%) | Memory: 58.3 MB (42.68%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    const str = String(x);
+    let reversedStr = '';
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedStr += str[i];
+    }
+    
+    return str === reversedStr;
+};

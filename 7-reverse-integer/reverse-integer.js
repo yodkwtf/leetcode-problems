8412395526a1d50e11f9c6609aae2b9fdc5a654a
Durgesh <48durgesh.kumar@gmail.com>
2024-03-28 02:17:31 +0530

    Time: 74 ms (57.48%) | Memory: 52.8 MB (57.89%) - LeetSync

--- 7-reverse-integer/reverse-integer.js
+++ 7-reverse-integer/reverse-integer.js
@@ -3,14 +3,6 @@
  * @return {number}
  */
 var reverse = function (x) {
-    const str = x.toString();
-    let reverseStr = '';
-
-    for (let i = str.length - 1; i >= 0; i--) {
-        reverseStr += str[i];
-    }
-
-    const reveresedMod = parseInt(reverseStr)
-
+    const reveresedMod = parseInt(x.toString().split('').reverse().join(''))
     return reveresedMod > 2 ** 31 - 1 ? 0 : reveresedMod * Math.sign(x);
 };
